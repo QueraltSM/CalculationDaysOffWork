facd796c6330980622639cc35771b07f201d4cd3
QueraltSM <queraltsosa@hotmail.com>
2017-07-14 13:19:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,43 @@
-}
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+string calculo (string  fechaBaja, string  fechaAlta) {
+    char diaBaja[2] = "";
+    int i  = 0;
+    while (fechaBaja[i] != '/')  {
+             diaBaja[i] += fechaBaja[i];
+             i++;
+    }
+    
+    char diaAlta[2] = ""; 
+    int j = 0;
+    while (fechaAlta[j] != '/') {
+            diaAlta[j] += fechaAlta[j];
+             j++;
+    }
+    
+    int diaBajaInteger = atoi(diaBaja);
+    int diaAltaInteger = atoi(diaAlta);
+    
+    int diasDiferencia = 0;
+    if (diaBajaInteger >= diaAltaInteger) {
+             diasDiferencia = diaBajaInteger - diaAltaInteger;
+    } else  {
+            diasDiferencia =(30-diaBajaInteger) +diaAltaInteger;
+    }
+    
+   
+    char mesBaja[2] = "";
+     int k = 0;
+     i++;
+    while (fechaBaja[i] != '\0')  {
+            mesBaja[k] += fechaBaja[i];
+            i++;
+            k++;
+    }
+    
+    char mesAlta[2] = ""; 
     int u = 0;
     j++;
     while (fechaAlta[j] != '\0') {
@@ -10,13 +49,8 @@
     int mesBajaInteger = atoi(mesBaja);
     int mesAltaInteger = atoi(mesAlta);
     
-    int mesDiferencia = 0;
-    if (mesBajaInteger <= mesAltaInteger) {
-             mesDiferencia = mesAltaInteger - mesBajaInteger;
-    } else  {
-                mesDiferencia = mesBajaInteger - mesAltaInteger;
-    }
-    
+    int mesDiferencia =  mesDiferencia = mesBajaInteger -  mesAltaInteger;
+
          if (mesDiferencia == 0) {
                      return  "The employer was " + std::to_string(diasDiferencia) + " out of work.";
          }   
