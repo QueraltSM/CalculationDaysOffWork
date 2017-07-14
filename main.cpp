35a73bd8fbef731b10b51d22949ce7ba73ef0aa5
QueraltSM <queraltsosa@hotmail.com>
2017-07-14 13:05:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-string calculo (char* fechaBaja, char* fechaAlta) {
+string calculo (string  fechaBaja, string  fechaAlta) {
     char diaBaja[2] = "";
     int i  = 0;
     while (fechaBaja[i] != '/')  {
@@ -31,7 +31,7 @@ string calculo (char* fechaBaja, char* fechaAlta) {
     char mesBaja[2] = "";
      int k = 0;
      i++;
-    while (fechaBaja[i] != '/')  {
+    while (fechaBaja[i] != '\0')  {
             mesBaja[k] += fechaBaja[i];
             i++;
             k++;
@@ -40,7 +40,7 @@ string calculo (char* fechaBaja, char* fechaAlta) {
     char mesAlta[2] = ""; 
     int u = 0;
     j++;
-    while (fechaAlta[j] != '/') {
+    while (fechaAlta[j] != '\0') {
          mesAlta[u] += fechaAlta[j];
         j++;
         u++;
@@ -56,38 +56,6 @@ string calculo (char* fechaBaja, char* fechaAlta) {
                 mesDiferencia = mesBajaInteger - mesAltaInteger;
     }
     
-    
-        
-    char añoBaja[4] = "";
-     int p = 0;
-     i++;
-    while (fechaBaja[i] != '\0')  {
-            añoBaja[p] += fechaBaja[i];
-            i++;
-            p++;
-    }
-    
-    char añoAlta[4] = ""; 
-    int w = 0;
-    j++;
-     while (fechaAlta[j] != '\0') {
-            añoAlta[w] += fechaAlta[j];
-             j++;
-             w++;
-    }
-    
-    int añoBajaInteger = atoi(añoBaja);
-    int añoAltaInteger = atoi(añoAlta);
-    
-    int añoDiferencia = 0;
-    if (añoBajaInteger <= añoAltaInteger) {
-        añoDiferencia = añoAltaInteger - añoBajaInteger;
-    } else  {
-         añoDiferencia = añoBajaInteger - añoAltaInteger;
-    }
-    
-        if (añoDiferencia == 0) {
-    
          if (mesDiferencia == 0) {
                      return  "The employer was " + std::to_string(diasDiferencia) + " out of work.";
          }   
@@ -97,23 +65,11 @@ string calculo (char* fechaBaja, char* fechaAlta) {
             }
         
                     return  "The employer was " + std::to_string(mesDiferencia) + " months and " + std::to_string(diasDiferencia) + "  days  out of work.";
-    
-            } 
-    
-           if (mesDiferencia == 0) {
-                     return  "The employer was " + std::to_string(añoDiferencia) + " years and " + std::to_string(diasDiferencia) + " days out of work.";
-             }
-    
-            if (diasDiferencia == 0) {
-                    return  "The employer was " + std::to_string(añoDiferencia) + " years and " + std::to_string(mesDiferencia) + " months out of work.";
-            }
-    
-         return "The employer was " + std::to_string(añoDiferencia) + " years, "  + std::to_string(mesDiferencia) + " months and " + std::to_string(diasDiferencia) + "  days out of work.";
-  
-}
+   
+  } 
 
 
 int  main(int argc, char** argv) {    
-        cout << calculo("17/05/1997", "11/04/1998")  << endl;
+        cout << calculo("17/05", "11/04")  << endl;
         return 0;      
 }
